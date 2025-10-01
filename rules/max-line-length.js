3a4b12c00c672aa1a6f9d976b1572a8a9051cecf
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-09-30 21:55:51 -0300

    Create max-line-length.js

---
+++ rules/max-line-length.js
@@ -0,0 +1,18 @@
+const MAX_LENGTH = 80;
+
+export function check(code, ast,filePath) {
+  const issues = [];
+  const lines = code.split("\n");
+  lines.forEach((line, index) => {
+    if (line.length > MAX_LENGTH) {
+      issues.push({
+        message: `\x1b[31mLine too long (${line.length} characters). Máx: ${MAX_LENGTH}\x1b[0m`,
+        line: index + 1,
+        column: MAX_LENGTH,
+        file: filePath
+      });
+    }
+  });
+
+  return issues;
+}
