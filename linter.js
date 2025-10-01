8f8c186d32c858d3a53994e23cc5d6e5367d31e8
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-09-30 21:56:00 -0300

    Create linter.js

---
+++ linter.js
@@ -0,0 +1,22 @@
+import fs from "fs";
+import esprima from "esprima";
+import * as noVar from "./rules/no-var.js";
+import * as maxLineLength from "./rules/max-line-length.js";
+import * as noConsole from "./rules/no-console.js";
+import * as noEmptyFunction from "./rules/no-empty-function.js";
+
+const rules = [noVar, maxLineLength, noConsole, noEmptyFunction];
+
+export function lintFile(filePath) {
+  const code = fs.readFileSync(filePath, "utf-8");
+  const ast = esprima.parseScript(code, { loc: true, tokens: true });
+
+  const issues = [];
+
+  for (const rule of rules) {
+    const foundIssues = rule.check(code, ast, filePath);
+    issues.push(...foundIssues);
+  }
+
+  return issues;
+}
