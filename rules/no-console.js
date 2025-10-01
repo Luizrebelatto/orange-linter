05a8445922f94f120b963dbc407e3c32dcab0784
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-09-30 21:55:48 -0300

    Create no-console.js

---
+++ rules/no-console.js
@@ -0,0 +1,20 @@
+export function check(code, ast, filePath) {
+  const issues = [];
+
+  ast.tokens.forEach((token, i) => {
+    if (
+      token.type === "Identifier" &&
+      token.value === "console" &&
+      ast.tokens[i + 1]?.value === "."
+    ) {
+      issues.push({
+        message: "\x1b[31mavoid use console.log\x1b[0m",
+        line: token.loc.start.line,
+        column: token.loc.start.column,
+        file: filePath
+      });
+    }
+  });
+
+  return issues;
+}
