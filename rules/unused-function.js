021c11a2f32a964899ea655c56b76cb206dc27fb
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-10-08 23:39:51 -0300

    Create unused-function.js

---
+++ rules/unused-function.js
@@ -0,0 +1,51 @@
+export function check(code, ast, filePath) {
+  const functionDeclared = new Set();
+  const functionsCalled = new Set();
+  const issues = [];
+
+  function traverse(node) {
+    if (!node || typeof node !== "object") return;
+
+    switch (node.type) {
+      case "FunctionDeclaration":
+        if (node.id?.name) {
+            functionDeclared.add(node.id.name)
+        };
+        break;
+
+      case "VariableDeclarator":
+        if (
+          (node.init?.type === "FunctionExpression" ||
+           node.init?.type === "ArrowFunctionExpression") &&
+          node.id?.name
+        ) {
+          functionDeclared.add(node.id.name);
+        }
+        break;
+
+      case "CallExpression":
+        if (node.callee?.type === "Identifier") {
+          functionsCalled.add(node.callee.name);
+        }
+        break;
+    }
+
+    for (const value of Object.values(node)) {
+      if (Array.isArray(value)) value.forEach(traverse);
+      else if (typeof value === "object") traverse(value);
+    }
+  }
+
+  traverse(ast);
+
+  for (const name of functionDeclared) {
+    if (!functionsCalled.has(name)) {
+      issues.push({
+        message: `Function '${name}' declared but never used`,
+        file: filePath,
+      });
+    }
+  }
+
+  return issues;
+}
